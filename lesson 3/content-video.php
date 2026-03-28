396f64a6c15e3c29d9ca9c0258e6a01780586e17
YllAbazii <dielli.abazi@gmail.com>
2026-03-28 14:09:30 +0100

    Lesson 3/4

---
+++ lesson 3/content-video.php
@@ -0,0 +1,5 @@
+<article>  <?php post_class():?>
+<h2><?php the_title();?></h2>
+<?php the_content():?>
+
+</article>
---
+++ lesson 3/page-ds-protofolio.php
@@ -0,0 +1,12 @@
+<?php     get_header();?>
+
+
+<h1>About Me Template(slug based).2</h1>
+
+
+<?php if (have_posts()) : while(have_posts()) : the_posts();?>
+<?php the_content();?>
+
+<?php endwhile; endif;?>
+
+<?php get_footer();?>
